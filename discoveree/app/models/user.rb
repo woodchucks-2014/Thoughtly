bad0a5c443bf289e25eeb344ce5f4135402e1adf
Natalie Baer <ncbaer@gmail.com>
2014-07-24 15:28:57 -0400

    setup for potential oauth implementation

--- discoveree/app/models/user.rb
+++ discoveree/app/models/user.rb
@@ -1,7 +1,8 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+  # :confirmable, :lockable, :timeoutable
+  devise :database_authenticatable, :registerable, #:omniauthable, #omniauthable is for oauth for google
+         :recoverable, :rememberable, :trackable, :validatable, #:omniauth_providers => [:google_oauth2] #oauth
+         #:provider, :uid #provider and uid are for oauth
 
 end
