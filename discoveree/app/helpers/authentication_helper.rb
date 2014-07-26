8f12ee872c52fd62266acf342373be01b2af7c69
adriansoghoian <adriansoghoian@gmail.com>
2014-07-26 11:46:32 -0400

    Adjusted authentication helper.

--- discoveree/app/helpers/authentication_helper.rb
+++ discoveree/app/helpers/authentication_helper.rb
@@ -8,12 +8,9 @@ module AuthenticationHelper
   end
 
   def ensure_admin_signed_in
-    # Redirect to the Google Account chooser if they're not logged in
     if !signed_in?
       session[:redirect_to] = request.url
       redirect_to new_session_path
-
-    # Redirect to the homepage if they don't have permission to do what they're doing
     elsif User.find(session[:user_id]).role != "Administrator"
       redirect_to root_path
     end
