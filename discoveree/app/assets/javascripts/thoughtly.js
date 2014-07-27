916b924b893cb9f36d9dc9fdc2964b4435044b7a
Natalie Baer <ncbaer@gmail.com>
2014-07-27 10:25:07 -0400

    changed colors to sass variables and shades

--- discoveree/app/assets/javascripts/thoughtly.js
+++ discoveree/app/assets/javascripts/thoughtly.js
@@ -1,8 +1,8 @@
-$(window).on("scroll", function () {
-  if ($(this).scrollTop() > 15) {
-      $("header").fadeIn().removeClass("show");
-  }
-  else {
-      $("header").fadeOut().addClass("show");
-  }
-});          
+// $(window).on("scroll", function () {
+//   if ($(this).scrollTop() > 15) {
+//       $("header").fadeIn().removeClass("show");
+//   }
+//   else {
+//       $("header").fadeOut().addClass("show");
+//   }
+// });          
