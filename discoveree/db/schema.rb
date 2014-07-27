0d3baa301b5d99af5140b6ccabdadccd49d8926a
adriansoghoian <adriansoghoian@gmail.com>
2014-07-27 17:00:20 -0400

    Bux fixes,tweaks, etc.

--- discoveree/db/schema.rb
+++ discoveree/db/schema.rb
@@ -16,16 +16,6 @@ ActiveRecord::Schema.define(version: 20140727015201) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "airports", force: true do |t|
-    t.string   "country"
-    t.string   "code"
-    t.float    "lat"
-    t.float    "long"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-    t.string   "name"
-  end
-
   create_table "categories", force: true do |t|
     t.string   "name"
     t.datetime "created_at"
@@ -38,30 +28,6 @@ ActiveRecord::Schema.define(version: 20140727015201) do
     t.string   "source"
     t.integer  "user_id"
     t.integer  "category_id"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  create_table "flights", force: true do |t|
-    t.string   "beginning_airport"
-    t.string   "ending_airport"
-    t.integer  "user_id"
-    t.string   "phone_number"
-    t.datetime "start_vacation"
-    t.datetime "end_vacation"
-    t.datetime "search_end"
-    t.integer  "duration"
-    t.integer  "price"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  create_table "notifications", force: true do |t|
-    t.string   "type",       default: "text"
-    t.string   "url",                         null: false
-    t.boolean  "notified",   default: false
-    t.integer  "user_id"
-    t.integer  "flight_id"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
