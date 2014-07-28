253106c871f983bc24ff122004b506f2d6a1ebeb
Natalie Baer <ncbaer@gmail.com>
2014-07-28 19:47:56 -0400

    edits

--- discoveree/app/assets/javascripts/thoughtly.js
+++ discoveree/app/assets/javascripts/thoughtly.js
@@ -2,7 +2,7 @@ $(function(){
 	$('#get_started').click(function(e){
 		e.preventDefault();
 		$.ajax({
-			url: "/users/signup",
+			url: "/users/signin",
 			data: $("form").serialize(),
 			success: function(data){
 				$("#welcome").html(data);
@@ -12,17 +12,17 @@ $(function(){
 
 })
 
-$(function(){
-	$('#welcome').on("click", '.event', function(e){
-		e.preventDefault();
-		$.ajax({
-			type: "GET",
-			url: "/users/signup",
-			data: $("form").serialize(),
-			success: function(data){
-				$("#welcome").html(data)
-			}
-		})
-	})
+// $(function(){
+// 	$('#welcome').on("click", '.event', function(e){
+// 		e.preventDefault();
+// 		$.ajax({
+// 			type: "GET",
+// 			url: "/users/signup",
+// 			data: $("form").serialize(),
+// 			success: function(data){
+// 				$("#welcome").html(data)
+// 			}
+// 		})
+// 	})
 	
-})
+// })
