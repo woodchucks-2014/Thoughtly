30c9800666b39a51d19d50874751b5813cced584
adriansoghoian <adriansoghoian@gmail.com>
2014-07-28 16:08:49 -0400

    Adding functionality to controller to send back link; not finished yet.

--- discoveree/app/controllers/categories_controller.rb
+++ discoveree/app/controllers/categories_controller.rb
@@ -11,20 +11,21 @@ class CategoriesController < ApplicationController
   end
 
   def create
-    @user = User.authenticate(params[:email], params[:password])
+    @user = User.authenticate(params[:extension_email], params[:extension_password])
     if @user
       @category_array = Category.analyze_url(params[:url])
       @related_categories = Category.format_related(@category_array)
       @category = Category.new(name: @category_array[0], related_categories: @related_categories)
-      render :json => { message: "Creating a briefing on: " + @category.name + "..." }
     else
       render :json => { message: "Oops! Looks like you need to sign up first." }
     end
     Content.generate(@category, @user) if @category.save
+    render :json => { message: "Created a briefing on: " + @category.name + ". ", anchor: "http://127.0.0.1:3000#{user_category_path(@user, @category)}" }
   end
 
   def show
     @current_user = User.find_by_id(session[:user_id])
+    p @current_user
     @category = Category.find_by_id(params[:id])
     @summary = @category.generate_summary
     unless @current_user.id == params[:user_id].to_i
