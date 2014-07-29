26ae0d962647435c7fd0a7d0cfae16035da2381d
Richard <RJNY86@gmail.com>
2014-07-28 21:01:04 -0400

    bcrypt

--- discoveree/db/schema.rb
+++ discoveree/db/schema.rb
@@ -34,7 +34,7 @@ ActiveRecord::Schema.define(version: 20140727015201) do
 
   create_table "users", force: true do |t|
     t.string   "email"
-    t.string   "password"
+    t.string   "password_digest"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
