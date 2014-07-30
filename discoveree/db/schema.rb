968db12f8749c14e4296d48cc7c116b180764364
adriansoghoian <adriansoghoian@gmail.com>
2014-07-30 11:07:33 -0400

    Added quick fix for double render error.

--- discoveree/db/schema.rb
+++ discoveree/db/schema.rb
@@ -36,7 +36,7 @@ ActiveRecord::Schema.define(version: 20140728233222) do
 
   create_table "users", force: true do |t|
     t.string   "email"
-    t.string   "password"
+    t.string   "password_digest"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
